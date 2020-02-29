587a9f11ff9a3d0ef8c9fefa3f722e69f6e75bd3
Muruges <muruges95@hotmail.com>
2020-02-29 12:11:40 +0800

    added more tests

--- src/__tests__/typeChecker.test.ts
+++ src/__tests__/typeChecker.test.ts
@@ -48,14 +48,26 @@ describe('binary expressions', () => {
     expect(() => typeCheck(program)).toThrowError()
   })
 
-  it()
+  it.skip('Allows for variables declared later, as long as function not called yet', () => {
+    const context = createContext(1)
+    const code = "const a = () => {return x + 1;}; const x = 3;"
+    const program = parse(code, context)
+    expect(() => typeCheck(program)).not.toThrowError()
+  })
+
+  it.skip('Type checks variables declared later', () => {
+    const context = createContext(1)
+    const code = "const a = () => {return x + 1;}; const x = 'b';"
+    const program = parse(code, context)
+    expect(() => typeCheck(program)).toThrowError()
+  })
 
   // NOTE currently fails, can fix once we introduce polymorphic types
-  // it('no errors when comparing string with string', () => {
-  //     const context = createContext(1)
-  //     const code = "const x = 'test'; const y = 'foo'; const z = x === y;"
-  //     const program = parse(code, context);
-  //     typeCheck(program);
-  //     expect(() => typeCheck(program)).not.toThrowError()
-  // })
+  it.skip('no errors when comparing string with string', () => {
+      const context = createContext(1)
+      const code = "const x = 'test'; const y = 'foo'; const z = x === y;"
+      const program = parse(code, context);
+      typeCheck(program);
+      expect(() => typeCheck(program)).not.toThrowError()
+  })
 })
